a132ee3f5e53082e283fbb76c6d74509b394f319
polthm <pol.thomas56@gmail.com>
2023-12-11 23:17:25 +0100

    feat: add reload prompt for PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { Box } from '@mui/material';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { registerSW } from 'virtual:pwa-register';
 
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import RouterContent from '@/components/RouterContent';
 
 const App: FC = () => {
+  useEffect(() => {
+    const updateSW = registerSW({
+      onNeedRefresh() {
+        if (
+          window.confirm(
+            "Une nouvelle version est disponible. Voulez-vous recharger pour l'utiliser ?"
+          )
+        ) {
+          window.location.reload();
+        }
+      },
+    });
+
+    return () => {
+      updateSW();
+    };
+  }, []);
+
   return (
     <Box
       display="flex"
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+declare module 'virtual:pwa-register' {
+  interface SWRegisterOptions {
+    onNeedRefresh?: () => void;
+  }
+
+  export function registerSW(options?: SWRegisterOptions): () => void;
+}
