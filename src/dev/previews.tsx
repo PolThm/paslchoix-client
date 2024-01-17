63d9c6e0cc85cbf6aee88f044dc3c5888c9bd6dc
polthm <pol.thomas56@gmail.com>
2024-01-17 14:42:30 +0100

    WIP

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,8 +1,24 @@
-import { Previews } from '@react-buddy/ide-toolbox';
+import { ComponentPreview, Previews } from '@react-buddy/ide-toolbox';
+
+import App from '@/App';
+
 import { PaletteTree } from './palette';
+import HomePage from '@/pages/HomePage';
 
 const ComponentPreviews = () => {
-  return <Previews palette={<PaletteTree />}></Previews>;
+  return (
+    <Previews palette={<PaletteTree />}>
+      <ComponentPreview path="/App">
+        <App />
+      </ComponentPreview>
+      <ComponentPreview path="/ComponentPreviews">
+        <ComponentPreviews />
+      </ComponentPreview>
+      <ComponentPreview path="/HomePage">
+        <HomePage />
+      </ComponentPreview>
+    </Previews>
+  );
 };
 
 export default ComponentPreviews;
